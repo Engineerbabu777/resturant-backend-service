1958ffdb7a01cf8ec827ccf1597a945d9124576e
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-04-10 22:20:02 +0500

    add: gin logger

--- main.go
+++ main.go
@@ -1,15 +1,22 @@
-package main;
+package main
 
+import (
+	"fmt"
+	"os"
 
+	"github.com/gin-gonic/gin"
+)
 
+func main() {
+	port := os.Getenv("PORT")
 
-func main(){
-	port := os.Getenv("PORT");
-
-	if port == ""{
-		port = "8080";
-		fmt.Println("INFO: No PORT environment variable detected, defaulting to " + port);
+	if port == "" {
+		port = "8080"
+		fmt.Println("INFO: No PORT environment variable detected, defaulting to " + port)
 	}
 
-	
+	router := gin.New()
+
+	router.Use(gin.Logger())
+
 }

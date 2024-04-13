a47035f2a8fae3a7e447aa427de53d380cc75b6d
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-04-13 10:37:06 +0500

    add: GetInvoices controller

--- controllers/invoiceController.go
+++ controllers/invoiceController.go
@@ -1,30 +1,63 @@
-
-
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"log"
+	"net/http"
+	"resturant-backend/database"
+	"time"
 
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
-func GetInvoices() gin.HandlerFunc{
-	return func(c *gin.Context){
+type InvoiceViewFormat struct {
+	Invoice_id       string
+	Payment_method   string
+	Order_id         string
+	Payment_status   *string
+	Payment_due      interface{}
+	Table_number     interface{}
+	Payment_due_date time.Time
+	Order_details    interface{}
+}
+
+var invoiceCollection *mongo.Collection = database.OpenCollection(database.Client, "invoice")
+
+func GetInvoices() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)  
+
+		result, err := invoiceCollection.Find(context.TODO(), bson.M{})
+		defer cancel();
+
+		if err != nil {
+			c.JSON(504, gin.H{"error": err.Error()})
+		}
+
+		var allInvoices []bson.M;
+		if err = result.All(ctx, &allInvoices); err!=nil {
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOK, allInvoices)
+	}
+}
+ 
+func GetInvoice() gin.HandlerFunc {
+	return func(c *gin.Context) {
 
 	}
 }
 
-func GetInvoice() gin.HandlerFunc{
-	return func(c *gin.Context){
-		
+func CreateInvoice() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
 	}
 }
 
-func CreateInvoice() gin.HandlerFunc{
-	return func(c *gin.Context){
-		
-	}
-}
+func UpdateInvoice() gin.HandlerFunc {
+	return func(c *gin.Context) {
 
-func UpdateInvoice() gin.HandlerFunc{
-	return func(c *gin.Context){
-		
 	}
 }

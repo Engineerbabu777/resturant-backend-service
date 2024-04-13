98df64efe805cd1cd0afa529ae9cf805a0f7235d
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-04-13 10:46:18 +0500

    add: GetInvoice

--- controllers/invoiceController.go
+++ controllers/invoiceController.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"resturant-backend/database"
+	"resturant-backend/models"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -46,7 +47,36 @@ func GetInvoices() gin.HandlerFunc {
  
 func GetInvoice() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)  
+		invoiceId := c.Param("invoice_id")
+		var invoice models.Invoice
 
+		err := foodCollection.FindOne(ctx, bson.M{"invoice_id": invoiceId}).Decode(&invoice)
+		defer cancel()
+		if err != nil {
+			c.JSON(504, gin.H{"error": err.Error()})
+		}
+
+		var invoiceView InvoiceViewFormat;
+
+		allOrderItems, err := ItemsByOrder(invoice.Order_id);
+		invoiceView.Order_id = invoice.Order_id;
+		invoiceView.Payment_due_date = invoice.Payment_due_date;
+
+		invoiceView.Payment_method = "null";
+		if invoice.Payment_method != nil {
+			invoiceView.Payment_method = *invoice.Payment_method
+		}
+
+		invoiceView.Invoice_id = invoice.Invoice_id;
+		invoiceView.Payment_status = *&invoice.Payment_status
+		invoiceView.Payment_due = allOrderItems[0]["payment_due"]
+		invoiceView.Table_number =allOrderItems[0]["table_number"]
+		invoiceView.Order_details = allOrderItems[0]["order_items"]
+
+
+
+		c.JSON(200, invoiceView)
 	}
 }
 

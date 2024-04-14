29ff4d98711e9d75a2d9a0a6fd34fe9dc91b6215
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-04-14 18:39:49 +0500

    add: UpdateInvoice controller annded

--- controllers/invoiceController.go
+++ controllers/invoiceController.go
@@ -10,7 +10,9 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type InvoiceViewFormat struct {
@@ -28,26 +30,26 @@ var invoiceCollection *mongo.Collection = database.OpenCollection(database.Clien
 
 func GetInvoices() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)  
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 
 		result, err := invoiceCollection.Find(context.TODO(), bson.M{})
-		defer cancel();
+		defer cancel()
 
 		if err != nil {
 			c.JSON(504, gin.H{"error": err.Error()})
 		}
 
-		var allInvoices []bson.M;
-		if err = result.All(ctx, &allInvoices); err!=nil {
+		var allInvoices []bson.M
+		if err = result.All(ctx, &allInvoices); err != nil {
 			log.Fatal(err)
 		}
 		c.JSON(http.StatusOK, allInvoices)
 	}
 }
- 
+
 func GetInvoice() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)  
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 		invoiceId := c.Param("invoice_id")
 		var invoice models.Invoice
 
@@ -57,24 +59,22 @@ func GetInvoice() gin.HandlerFunc {
 			c.JSON(504, gin.H{"error": err.Error()})
 		}
 
-		var invoiceView InvoiceViewFormat;
+		var invoiceView InvoiceViewFormat
 
-		allOrderItems, err := ItemsByOrder(invoice.Order_id);
-		invoiceView.Order_id = invoice.Order_id;
-		invoiceView.Payment_due_date = invoice.Payment_due_date;
+		allOrderItems, err := ItemsByOrder(invoice.Order_id)
+		invoiceView.Order_id = invoice.Order_id
+		invoiceView.Payment_due_date = invoice.Payment_due_date
 
-		invoiceView.Payment_method = "null";
+		invoiceView.Payment_method = "null"
 		if invoice.Payment_method != nil {
 			invoiceView.Payment_method = *invoice.Payment_method
 		}
 
-		invoiceView.Invoice_id = invoice.Invoice_id;
+		invoiceView.Invoice_id = invoice.Invoice_id
 		invoiceView.Payment_status = *&invoice.Payment_status
 		invoiceView.Payment_due = allOrderItems[0]["payment_due"]
-		invoiceView.Table_number =allOrderItems[0]["table_number"]
+		invoiceView.Table_number = allOrderItems[0]["table_number"]
 		invoiceView.Order_details = allOrderItems[0]["order_items"]
-
-
 
 		c.JSON(200, invoiceView)
 	}
@@ -82,12 +82,81 @@ func GetInvoice() gin.HandlerFunc {
 
 func CreateInvoice() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 
+		var invoice models.Invoice
+
+		if err := c.BindJSON(&invoice); err != nil {
+			c.JSON(504, gin.H{"error": err.Error()})
+			return
+		}
+
+		var order models.Order
+
+		err := orderCollection.FindOne(ctx, bson.M{"order_id": invoice.Order_id}).Decode(&order)
+		defer cancel()
+
+		if err != nil {
+			c.JSON(504, gin.H{"error": err.Error()})
+			return
+		}
 	}
 }
 
 func UpdateInvoice() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+
+		var invoice models.Invoice
+		invoiceId := c.Param("invoice_id")
+
+		if err := c.BindJSON(&invoice); err != nil {
+			c.JSON(504, gin.H{"error": err.Error()})
+			return
+		}
+
+		filter := bson.M{"invoice_id": invoiceId}
+
+		var updateObj primitive.D
+
+		if invoice.Payment_method != nil {
+			updateObj = append(updateObj, bson.E{"payment_method", invoice.Payment_method})
+		}
+		if invoice.Payment_status != nil {
+			updateObj = append(updateObj, bson.E{"payment_status", invoice.Payment_status})
+		}
+
+		invoice.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		updateObj = append(updateObj, bson.E{"updated_at", invoice.Updated_at})
+		upsert := true
+		filter = bson.M{"order_id": invoiceId}
+
+		opt := options.UpdateOptions{
+			Upsert: &upsert,
+		}
+
+		status := "PENDING"
+
+		if invoice.Payment_status == nil {
+			invoice.Payment_status = &status
+		}
+
+		result, err := invoiceCollection.UpdateOne(
+			ctx,
+			filter,
+			bson.D{
+				{"$set", updateObj},
+			},
+			&opt,
+		)
+
+		if err != nil {
+			c.JSON(504, gin.H{"error": err.Error()})
+			return
+		}
+
+		defer cancel()
+		c.JSON(200, result)
 
 	}
 }

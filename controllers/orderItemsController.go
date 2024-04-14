c6e57945b4f7fd692c7d19e802c5dd9db6e74e55
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-04-14 19:41:15 +0500

    add: OrderItemPack struct

--- controllers/orderItemsController.go
+++ controllers/orderItemsController.go
@@ -1,43 +1,50 @@
 package controllers
 
 import (
+	"resturant-backend/database"
+	"resturant-backend/models"
+
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
+type OrderItemPack struct {
+	Table_id    *string
+	Order_items []models.OrderItem
+}
 
-func GetOrderItems() gin.HandlerFunc{
-	return func(c *gin.Context){
+
+func GetOrderItems() gin.HandlerFunc {
+	return func(c *gin.Context) {
 
 	}
 }
 
-func GetOrderItemByOrder() gin.HandlerFunc{
-	return func(c *gin.Context){
-		
-	}
-}
-
-
-
-func UpdateOrderItem() gin.HandlerFunc{
-	return func(c *gin.Context){
-		
-	}
-}
-
-func ItemsByOrder(id string) (OrderItems []primitive.M,err error){
-
-}
-
-func GetOrderItem() gin.HandlerFunc{
-	return func(c *gin.Context){
+func GetOrderItemByOrder() gin.HandlerFunc {
+	return func(c *gin.Context) {
 
 	}
 }
 
-func CreateOrderItem() gin.HandlerFunc{
-	return func(c *gin.Context){
+func UpdateOrderItem() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func ItemsByOrder(id string) (OrderItems []primitive.M, err error) {
+
+}
+
+func GetOrderItem() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func CreateOrderItem() gin.HandlerFunc {
+	return func(c *gin.Context) {
 
 	}
 }

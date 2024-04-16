7031fcf24f0c7153b0212aefea0a99c616ca6b0c
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-04-16 19:00:29 +0500

    Add: GetTable

--- controllers/tableController.go
+++ controllers/tableController.go
@@ -4,12 +4,11 @@ import (
 	"context"
 	"log"
 	"net/http"
-	"resturant-backend/database"
+	"resturant-backend/models"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
 )
 
 
@@ -32,7 +31,16 @@ func GetTables() gin.HandlerFunc{
 
 func GetTable() gin.HandlerFunc{
 	return func(c *gin.Context){
-		
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		tableId := c.Param("table_id")
+		var table models.Table
+
+		err := tableCollection.FindOne(ctx, bson.M{"table_id": tableId}).Decode(&table)
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while fetching the tables"})
+		}
+		c.JSON(http.StatusOK, table)
 	}
 }
 
